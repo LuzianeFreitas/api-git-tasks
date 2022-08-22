0885245666dfeeb2d43cb03ccba7cab56dd3143f
LuzianeFreitas07 <luziane.freitas@convenia.com.br>
2022-08-22 10:30:26 -0300

    add: tasks service

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { makeExecutableSchema } = require('@graphql-tools/schema')
 const { typeDefs, resolvers } = require('./src/graphql')
 const GitHubService = require('./src/services/GitHub.service')
 const UserRegisterService = require('./src/services/UserRegisterService')
+const TasksRegisterService = require('./src/services/TasksRegisterService')
 
 const schema = makeExecutableSchema({
     typeDefs,
@@ -13,7 +14,8 @@ const server = new ApolloServer({
     schema,
     dataSources: () => ({
         gitHubService: GitHubService,
-        userRegisterService: UserRegisterService
+        userRegisterService: UserRegisterService,
+        tasksRegisterService: TasksRegisterService
     })
 })
 
--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,6 +1,11 @@
 module.exports = {
+    User: {
+        async tasks (user,_, { dataSources }) {
+            return await dataSources.tasksRegisterService.getTasks(user.id)
+        }
+    },
     Query: {
-        async user(_, { login }, { dataSources }) {
+        async user (_, { login }, { dataSources }) {
             const userFound = await dataSources.userRegisterService.getUserByLogin(login)
 
             if (userFound) return userFound
